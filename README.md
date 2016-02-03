e9280e6fa3ca3d65de43db1f69ec51ec93c14199
MutterPedro <pedro.mutter@gmail.com>
2016-02-02 22:59:40 -0200

    string A3

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# TPGerenciaDeConfig
+# Trabalho sobre gerencia de versões
+
+GCC120 - Engenharia de Software 2
+
+Felipe Silva Dias
+Pedro de Almeida Mutter
--- src/br/com/a/A.java
+++ src/br/com/a/A.java
@@ -3,6 +3,7 @@ package br.com.a;
 public class A {
 	private int A1;
 	private float A2;
+	private String A3;
 	
 	public int getA1() {
 		return A1;
@@ -16,6 +17,14 @@ public class A {
 	public void setA2(float a2) {
 		A2 = a2;
 	}
+	public String getA3() {
+		return A3;
+	}
+	public void setA3(String a3) {
+		A3 = a3;
+	}
+	
+	
 	
 	
 }
