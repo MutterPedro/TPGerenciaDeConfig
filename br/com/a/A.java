89f3c86bc73918f0f132cb4a824b4745ea2ca409
MutterPedro <pedro.mutter@gmail.com>
2016-02-02 22:44:05 -0200

    v1.0

---
+++ br/com/a/A.java
@@ -0,0 +1,21 @@
+package br.com.a;
+
+public class A {
+	private int A1;
+	private float A2;
+	
+	public int getA1() {
+		return A1;
+	}
+	public void setA1(int a1) {
+		A1 = a1;
+	}
+	public float getA2() {
+		return A2;
+	}
+	public void setA2(float a2) {
+		A2 = a2;
+	}
+	
+	
+}
